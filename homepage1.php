26f83cf6e265dd9880f6b96aa3f72bc88c717b65
sami4007 <al.sami88744@gmail.com>
2026-01-16 23:40:52 +0600

    Admin Homepage

---
+++ homepage1.php
@@ -0,0 +1,38 @@
+<?php
+// Home Page
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Student Management Portal</title>
+
+    <link rel="stylesheet" href="assets/css/homepage.css">
+</head>
+<body>
+
+<!-- ===== NAVBAR ===== -->
+<nav class="navbar">
+    <a href="#">New Information</a>
+    <a href="#">New Entry</a>
+    <a href="#">Fee Details</a>
+    <a href="#">About</a>
+    <a href="home.php">Exit</a>
+</nav>
+
+<!-- ===== SLIDER SECTION ===== -->
+<div class="slider-container">
+
+    <div class="slide active" style="background-image: url('assets/images/slide1.png');"></div>
+    <div class="slide" style="background-image: url('assets/images/slide2.jpg');"></div>
+    <div class="slide" style="background-image: url('assets/images/slide3.jpg');"></div>
+
+    <div class="slider-title">
+        STUDENT MANAGEMENT PORTAL
+    </div>
+
+</div>
+
+<script src="assets/js/slider.js"></script>
+</body>
+</html>
