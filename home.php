012dfda77057aae15f284a159103d061f51cd98f
sami4007 <al.sami88744@gmail.com>
2026-01-16 23:34:53 +0600

    Added First WebPage

---
+++ home.php
@@ -0,0 +1,27 @@
+<?php
+// Home / Landing Page
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>VOLTECH | Welcome</title>
+    <link rel="stylesheet" href="assets/css/home.css">
+</head>
+<body>
+
+<div class="home-wrapper">
+
+    <div class="overlay-box">
+        <h1>Welcome to VolTech</h1>
+
+        <div class="button-group">
+            <a href="adminLogin.php" class="btn">Admin</a>
+            <a href="UserLogin.php" class="btn">User</a>
+        </div>
+    </div>
+
+</div>
+
+</body>
+</html>
