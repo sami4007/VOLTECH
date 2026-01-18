6ee713834a092b1e6eae99a0252f622c4d0866cc
Demo-Repository <benazirnewaz@gmail.com>
2026-01-18 23:04:56 +0600

    Added about3

---
+++ about3.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>About - Contributors</title>
+    <link rel="stylesheet" href="/Project/VOLTECH/assets/css/about.css">
+
+</head>
+<body>
+
+<div class="page about3">
+
+    <div class="center-box">
+        <h1>CONTRIBUTORS</h1>
+        <p>
+            BENAZIR NEWAZ KHAN : 23-50217-1<br><br>
+            SAIF AL SAMI : 23-50215-1<br><br>
+            MD. NAZRUL ISLAM NABIL : 23-50205-1
+        </p>
+    </div>
+
+    <button class="nav-btn back" onclick="goBack('about2.php')">Back</button>
+    <button class="nav-btn next" onclick="goNext('homepage1.php')">Next</button>
+
+
+</div>
+
+<script src="/Project/VOLTECH/assets/js/about.js"></script>
+
+</body>
+</html>
